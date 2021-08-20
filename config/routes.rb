47f96cbc9be96da808eb3c938d7ef91f2c065022
Tasso Rodrigues <tasso_henrique@hotmail.com>
2021-08-20 11:14:28 -0400

    mothafoca jones

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#show'
   resources :lists, only: [:index, :new, :show, :create] do
     resources :bookmarks, only: [:new, :create]
   end
